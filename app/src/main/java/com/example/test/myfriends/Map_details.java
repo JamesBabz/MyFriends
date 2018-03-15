fd77cb11a13efcc197d5d975fe698c6c3c3afccf
JamesBabz <web.fuhlendorff@gmail.com>
2018-03-15 11:41:08 +0100

    map update
    map sets marker from address

--- app/src/main/java/com/example/test/myfriends/Map_details.java
+++ app/src/main/java/com/example/test/myfriends/Map_details.java
@@ -1,32 +1,63 @@
 package com.example.test.myfriends;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.v4.app.FragmentActivity;
 
-public class Map_details extends AppCompatActivity {
+import com.example.test.myfriends.Entity.Friend;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Map_details extends FragmentActivity implements OnMapReadyCallback {
+
+    Friend friend;
+    private GoogleMap mMap;
+    Geocoder geocoder;
+
+
+    public Map_details() {
+        Location location = new Location("");
+        friend = new Friend(1,"Bent","Spangsbjerg møllevej 62b", location, 12345678, "test@test.com", "www.test.dk", "11-07-89", null);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_details);
-
-
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        geocoder = new Geocoder(this);
+        mMap = googleMap;
+        List<Address> address = null;
+
+        try {
+            address = geocoder.getFromLocationName(friend.getAddress(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng location = new LatLng(address.get(0).getLatitude(), address.get(0).getLongitude());
+        mMap.addMarker(new MarkerOptions().position(location).title("Marker in Sydney"));
+        mMap.moveCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+
+
 
 }
