cea797b81b3508f26805837cb0dfdb86496d68e6
JamesBabz <web.fuhlendorff@gmail.com>
2018-03-15 10:09:38 +0100

    created map view
    created the view for the map

---
+++ app/src/main/java/com/example/test/myfriends/MapDetails.java
@@ -0,0 +1,16 @@
+package com.example.test.myfriends;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by James on 15-03-2018.
+ */
+
+public class MapDetails extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_details);
+    }
+}
