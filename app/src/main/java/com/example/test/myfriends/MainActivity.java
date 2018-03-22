2f0d20dc0c8f8186eac6398298161657681f8a14
thomasmeyerh <thom953b@easv365.dk>
2018-03-22 09:20:46 +0100

    Top_Main_Menu

--- app/src/main/java/com/example/test/myfriends/MainActivity.java
+++ app/src/main/java/com/example/test/myfriends/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         friendService.setContext(this);
 
         listViewFriends = findViewById(R.id.listViewFriends);
-      //  createFriend();
+     // createFriend();
         allFriends = friendService.getAllFriends();
         listAdapter = new ListAdapter(this, R.layout.cell_extended, allFriends);
         listViewFriends.setAdapter(listAdapter);
@@ -53,6 +56,19 @@ public class MainActivity extends AppCompatActivity {
     public MainActivity() {
 
         friendService = FriendService.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        return true;
     }
 
     private void createFriend()
